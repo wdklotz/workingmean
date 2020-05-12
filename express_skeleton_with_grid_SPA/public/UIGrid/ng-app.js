2a820957a90236615f1874c8845d45ecf44ccc3e
Wolf-Dieter Klotz <wdklotz@gmail.com>
2020-05-12 23:38:15 +0200

    progressing

--- express_skeleton_with_grid_SPA/public/UIGrid/ng-app.js
+++ express_skeleton_with_grid_SPA/public/UIGrid/ng-app.js
@@ -197,106 +197,103 @@ myApp.controller('doc-edit-ctrl', ['$scope','DocService',function($scope,DocServ
     };
 }]);
 
-// RESTFUL data provider
-myApp.factory('DocService',['$resource', function($resource) {
+// RESTFUL data providers
+myApp.factory('DocService', ['$resource', function($resource) {
     return $resource('/api/lib/:id',
         {id:'@id'},
         {update: {method: 'PUT'}} // there is no HTTP PUT support available per default !!!
         );
 }]);
+myApp.factory('ItemService',['$resource', function($resource) {
+    let authorResource = $resource('/api/lib/authors');
+    let typeResource   = $resource('/api/lib/types');
+    let shelfResource  = $resource('/api/lib/shelfs');
+    let authors = {};
+    let types   = {};
+    let shelfs  = {};
+    return [{Authors:authors},{Types:types},{Shelfs:shelfs}];
+}]);
 
-/**
- * AngularJS default filter with the following expression:
- * "person in people | filter: {name: $select.search, age: $select.search}"
- * performs an AND between 'name: $select.search' and 'age: $select.search'.
- * We want to perform an OR.
- */
+// nofilter filter
 myApp.filter('propsFilter', function() {
   return function(items, props) {
     var out = [];
-
-    if (angular.isArray(items)) {
-      var keys = Object.keys(props);
-
-      items.forEach(function(item) {
-        var itemMatches = false;
-
-        for (var i = 0; i < keys.length; i++) {
-          var prop = keys[i];
-          var text = props[prop].toLowerCase();
-          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-            itemMatches = true;
-            break;
-          }
-        }
-
-        if (itemMatches) {
-          out.push(item);
-        }
-      });
-    } else {
-      // Let the output be the input untouched
-      out = items;
-    }
+    // Let the output be the input untouched
+    out = items;
     return out;
   };
 });
 
 myApp.controller('AuthCtrl', function ($scope, $http, $timeout, $interval) {
-  var vm = this;
+    var vm = this;
 
-  vm.disabled = false;
-  vm.searchEnabled = true;
-
-  vm.enable = function() {
     vm.disabled = false;
-  };
-
-  vm.disable = function() {
-    vm.disabled = true;
-  };
-
-  vm.enableSearch = function() {
     vm.searchEnabled = true;
-  };
 
-  vm.disableSearch = function() {
-    vm.searchEnabled = false;
-  };
+    /* // mock data */
+    vm.peopleObj = {
+    '1' :  { name: 'Adam',      id: 24},
+    '2' :  { name: 'Amalie',    id: 25},
+    '3' :  { name: 'Estefanía', id: 26},
+    '4' :  { name: 'Adrian',    id: 27},
+    '5' :  { name: 'Wladimir',  id: 28},
+    '6' :  { name: 'Samantha',  id: 29},
+    '7' :  { name: 'Nicole',    id: 30},
+    '8' :  { name: 'Natasha',   id: 31},
+    '9' :  { name: 'Michael',   id: 32},
+    '10' : { name: 'Nicolás',   id: 33}
+    };
+    vm.person = {};
+    vm.person.selectedValue = vm.peopleObj[1];
+    vm.person.selected = vm.person.selectedValue;
+});
 
-  vm.peopleObj = {
-    '1' : { name: 'Adam',      email: 'adam@email.com',      age: 12, country: 'United States' },
-    '2' : { name: 'Amalie',    email: 'amalie@email.com',    age: 12, country: 'Argentina' },
-    '3' : { name: 'Estefanía', email: 'estefania@email.com', age: 21, country: 'Argentina' },
-    '4' : { name: 'Adrian',    email: 'adrian@email.com',    age: 21, country: 'Ecuador' },
-    '5' : { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30, country: 'Ecuador' },
-    '6' : { name: 'Samantha',  email: 'samantha@email.com',  age: 30, country: 'United States' },
-    '7' : { name: 'Nicole',    email: 'nicole@email.com',    age: 43, country: 'Colombia' },
-    '8' : { name: 'Natasha',   email: 'natasha@email.com',   age: 54, country: 'Ecuador' },
-    '9' : { name: 'Michael',   email: 'michael@email.com',   age: 15, country: 'Colombia' },
-    '10' : { name: 'Nicolás',   email: 'nicolas@email.com',    age: 43, country: 'Colombia' }
-  };
+myApp.controller('TypeCtrl', function ($scope, $http, $timeout, $interval) {
+    var vm = this;
 
-  vm.person = {};
+    vm.disabled = false;
+    vm.searchEnabled = true;
 
-  vm.person.selectedValue = vm.peopleObj[3];
-  vm.person.selectedSingle = 'Samantha';
-  vm.person.selectedSingleKey = '5';
-  // To run the demos with a preselected person object, uncomment the line below.
-  //vm.person.selected = vm.person.selectedValue;
+    /* // mock data */
+    vm.peopleObj = {
+    '1' :  { name: 'Adam',      id: 24},
+    '2' :  { name: 'Amalie',    id: 25},
+    '3' :  { name: 'Estefanía', id: 26},
+    '4' :  { name: 'Adrian',    id: 27},
+    '5' :  { name: 'Wladimir',  id: 28},
+    '6' :  { name: 'Samantha',  id: 29},
+    '7' :  { name: 'Nicole',    id: 30},
+    '8' :  { name: 'Natasha',   id: 31},
+    '9' :  { name: 'Michael',   id: 32},
+    '10' : { name: 'Nicolás',   id: 33}
+    };
+    vm.person = {};
+    vm.person.selectedValue = vm.peopleObj[1];
+    vm.person.selected = vm.person.selectedValue;
+});
 
-  vm.people = [
-    { name: 'Adam',      email: 'adam@email.com',      age: 12, country: 'United States' },
-    { name: 'Amalie',    email: 'amalie@email.com',    age: 12, country: 'Argentina' },
-    { name: 'Estefanía', email: 'estefania@email.com', age: 21, country: 'Argentina' },
-    { name: 'Adrian',    email: 'adrian@email.com',    age: 21, country: 'Ecuador' },
-    { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30, country: 'Ecuador' },
-    { name: 'Samantha',  email: 'samantha@email.com',  age: 30, country: 'United States' },
-    { name: 'Nicole',    email: 'nicole@email.com',    age: 43, country: 'Colombia' },
-    { name: 'Natasha',   email: 'natasha@email.com',   age: 54, country: 'Ecuador' },
-    { name: 'Michael',   email: 'michael@email.com',   age: 15, country: 'Colombia' },
-    { name: 'Nicolás',   email: 'nicolas@email.com',    age: 43, country: 'Colombia' }
-  ];
+myApp.controller('ShelfCtrl', function ($scope, $http, $timeout, $interval) {
+    var vm = this;
+
+    vm.disabled = false;
+    vm.searchEnabled = true;
+
+    /* // mock data */
+    vm.peopleObj = {
+    '1' :  { name: 'Adam',      id: 24},
+    '2' :  { name: 'Amalie',    id: 25},
+    '3' :  { name: 'Estefanía', id: 26},
+    '4' :  { name: 'Adrian',    id: 27},
+    '5' :  { name: 'Wladimir',  id: 28},
+    '6' :  { name: 'Samantha',  id: 29},
+    '7' :  { name: 'Nicole',    id: 30},
+    '8' :  { name: 'Natasha',   id: 31},
+    '9' :  { name: 'Michael',   id: 32},
+    '10' : { name: 'Nicolás',   id: 33}
+    };
+    vm.person = {};
+    vm.person.selectedValue = vm.peopleObj[1];
+    vm.person.selected = vm.person.selectedValue;
 });
 
 })();
