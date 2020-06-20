02f37a051ee486f182167dc428824892bcdd56f0
Wolf-Dieter Klotz <wdklotz@gmail.com>
2020-06-20 19:46:55 +0200

    give up
    could not get tthe result I wanted with ATS controller

--- express_skeleton_with_grid_SPA/public/UIGrid/atsController.js
+++ express_skeleton_with_grid_SPA/public/UIGrid/atsController.js
@@ -3,25 +3,28 @@ angular.module('ngApp')
 .controller('atsController', ['$scope','$log','authResource','typeResource','shelfResource','U',function($scope, $log, authResource,typeResource,shelfResource,U) {
     const scope = $scope;
     const vm    = scope;
-    scope.a_titles = [];
-    scope.ar = 'rep';
-    scope.tr = 'rep';
-    scope.sr = 'rep';
+    scope.ar = 'ren';
+    scope.tr = 'ren';
+    scope.sr = 'ren';
+//     scope.titles = {auth:[],type:[],shelf:[]};
     // Authors
-    authResource.query([],function(value) {
+    scope.a_titles = [];
+    authResource.query([]).$promise.then(function(value) {
         vm.authTable = value;  //<-- that's the table from db
-        U.tbl_log('atsController#query#vm.authTable',vm.authTable);  // check
+//         U.tbl_log('atsController#query#vm.authTable',vm.authTable);  // check
         vm.authTable.forEach(item => scope.a_titles.push(item.Author));
+        U.tbl_log('atsController#query#scope.a_titles',scope.a_titles);  // check
+//         scope.a_titles = fillOptionsArray(vm.authTable, scope.ar);
     });
     // Types
     scope.t_titles = [];
-    typeResource.query([],function(value) {
+    typeResource.query([]).$promise.then(function(value) {
         vm.typeTable = value;  //<-- that's the table from db
         vm.typeTable.forEach(item => scope.t_titles.push(item.Type));
     });
     // Shelfs
     scope.s_titles = [];
-    shelfResource.query([],function(value) {
+    shelfResource.query([]).$promise.then(function(value) {
         vm.shelfTable = value;  //<-- that's the table from db
         vm.shelfTable.forEach(item => scope.s_titles.push(item.Shelf));
     });
@@ -29,4 +32,17 @@ angular.module('ngApp')
     scope.ats_submit = function() {
         console.log(scope.author,scope.type,scope.shelf);
     };
+
+    function fillOptionsArray(table,radioBtn) {
+        var titles = [];
+        switch (radioBtn) {
+            case 'add':
+                break;
+            case 'ren':
+                table.forEach(item=>titles.push(item));
+                break;
+            }
+        U.tbl_log('atsController#fillOptionsArray#titles',titles);  // check
+        return titles;
+    }
 }]); // atsController
